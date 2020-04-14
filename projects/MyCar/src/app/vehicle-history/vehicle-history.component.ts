79b85c4c8306050f868d7b915070b99948b065b8
Daniel Caspers <daniel@caspersonline.us>
2020-04-13 22:38:49 -0500

    feat(#23 - Vehicle History/Inspections/Billing) - Select vehicle in history via dropdown
    Added dropdown for selecting vehicle(s) in history. The initial state from the URL
    sets the dropdown selection and chooses which vehicles are in view.
    Following that, the select box allows further changes in case the vehicle the user
    selected upon initial navigation was incorrect.
    
    The vehicle list in the dropdown should be sorted by year, make, model, and license.
    Vehicles which do not yet have history (because they have no VIN) are omitted,

--- projects/MyCar/src/app/vehicle-history/vehicle-history.component.ts
+++ projects/MyCar/src/app/vehicle-history/vehicle-history.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { sortBy } from 'lodash-es';
 import { first } from 'rxjs/operators';
 
 import { VehicleHistoryEntry } from './vehicle-history.models';
 import { VehicleHistoryHttpService } from './vehicle-history-http.service';
+import { VehiclesHttpService } from '../my-vehicles/vehicles-http.service';
+import { VehicleOverview } from '../my-vehicles/vehicle.models';
 
 @Component({
 	selector: 'ma-vehicle-history',
@@ -14,16 +17,36 @@ import { VehicleHistoryHttpService } from './vehicle-history-http.service';
 export class VehicleHistoryComponent implements OnInit {
 
 	private vehicleId: string = null;
-
+	private myVehicles: VehicleOverview[] = [];
 	public vehicleHistory: VehicleHistoryEntry[] = [];
 
 	constructor(
 		private route: ActivatedRoute,
+		private vehiclesHttpService: VehiclesHttpService,
 		private vehicleHistoryService: VehicleHistoryHttpService
 	) { }
 
 	public ngOnInit(): void {
 		this.vehicleId = this.route.snapshot.paramMap.get('vehicleId');
+		this.getAllVehicles();
+		this.updateVehicleHistory();
+	}
+
+	public onSelectionChange(): void {
+		this.updateVehicleHistory();
+	}
+
+	private getAllVehicles(): void {
+		this.vehiclesHttpService.getVehiclesForClient()
+			.pipe(first())
+			.subscribe((vehicles) => {
+				// The vehicle list in the dropdown should be sorted by year, make, model, and license.
+				// Vehicles which do not yet have history (because they have no VIN) are omitted,
+				this.myVehicles = sortBy(vehicles, 'year', 'make', 'model', 'license').filter(v => !!v.vehicleID);
+			});
+	}
+
+	private updateVehicleHistory(): void {
 		const observable = !!this.vehicleId ?
 			this.vehicleHistoryService.getVehicleHistoryByVIN(this.vehicleId) :
 			this.vehicleHistoryService.getVehicleHistoryByClient();
@@ -36,5 +59,4 @@ export class VehicleHistoryComponent implements OnInit {
 				this.vehicleHistory = vehicleHistoryEntries;
 			});
 	}
-
 }
--- projects/MyCar/src/app/vehicle-history/vehicle-history.module.ts
+++ projects/MyCar/src/app/vehicle-history/vehicle-history.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import {
 	SafeUrlModule
@@ -12,6 +15,7 @@ import { VehicleHistoryRoutingModule } from './vehicle-history-routing.module';
 import { VehicleHistoryComponent } from './vehicle-history.component';
 import { VehicleHistoryHttpService } from './vehicle-history-http.service';
 
+
 @NgModule({
 	declarations: [ VehicleHistoryComponent ],
 	imports: [
@@ -20,6 +24,9 @@ import { VehicleHistoryHttpService } from './vehicle-history-http.service';
 
 		MatButtonModule,
 		MatExpansionModule,
+		MatFormFieldModule,
+		MatSelectModule,
+		MatToolbarModule,
 
 		SafeUrlModule,
 
