758a3bb3ce100fe9a4793c3e0b23b3b85842661b
Daniel Caspers <daniel@caspersonline.us>
2021-01-24 19:34:30 -0600

    chore(deployment) - re-introduce production environment

--- projects/MyCar/src/environments/environment.prod.ts
+++ projects/MyCar/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
-// TODO DJC Temporarily changing prod environment to staging for testing HTTP Delete feature issues for #50
 import { HttpHeaders } from '@angular/common/http';
 import { MyCarEnvironment } from './environment.models';
 
-const appKey = 'D4LgRtMpfyZKXBaYSgRzah6jBf3w7W0v';
+const appKey = 'vngL2peLDHaT2XgHUyry8vYv5lezpAlj';
+
 export const environment: MyCarEnvironment = {
-	production: false,
-	apiBaseUrl: 'https://d3-devel.murphyauto.net/api/v2',
-	oauthProviderUrl: `https://d3-devel.murphyauto.net/api/v2/auth/login?appkey=${appKey}`,
+	production: true,
+	apiBaseUrl: 'https://d3api.murphyauto.net/api/v2',
+	oauthProviderUrl: `https://d3api.murphyauto.net/api/v2/auth/login?appkey=${appKey}`,
 	httpOptions: {
 		headers: new HttpHeaders({
 			'Access-Control-Allow-Origin': '*',
@@ -17,31 +17,7 @@ export const environment: MyCarEnvironment = {
 		})
 	},
 	googleAnalytics: {
-		domain: 'none',
-		trackingId: 'UA-140002525-1',
+		domain: 'auto',
+		trackingId: 'UA-10667959-7'
 	}
 };
-
-// import { HttpHeaders } from '@angular/common/http';
-// import { MyCarEnvironment } from './environment.models';
-//
-// const appKey = 'vngL2peLDHaT2XgHUyry8vYv5lezpAlj';
-//
-// export const environment: MyCarEnvironment = {
-// 	production: true,
-// 	apiBaseUrl: 'https://d3api.murphyauto.net/api/v2',
-// 	oauthProviderUrl: `https://d3api.murphyauto.net/api/v2/auth/login?appkey=${appKey}`,
-// 	httpOptions: {
-// 		headers: new HttpHeaders({
-// 			'Access-Control-Allow-Origin': '*',
-// 			// 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PATCH',
-// 			// 'Access-Control-Allow-Headers': 'Authorization, x-appkey',
-//
-// 			'x-appkey': appKey
-// 		})
-// 	},
-// 	googleAnalytics: {
-// 		domain: 'auto',
-// 		trackingId: 'UA-10667959-7'
-// 	}
-// };
