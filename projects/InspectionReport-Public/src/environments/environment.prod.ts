14738c9fb53ba9a13f7cda745d20315687c8a084
Dan Caspers <dan@caspers.dev>
2022-02-22 20:14:31 -0600

    feat(#41 - Video Support) - Temporarily build in staging mode for test on CA2

--- projects/InspectionReport-Public/src/environments/environment.prod.ts
+++ projects/InspectionReport-Public/src/environments/environment.prod.ts
@@ -1,12 +1,21 @@
 import { HttpHeaders } from '@angular/common/http';
 import { MaEnvironmentBase } from '../../../../environment.model';
 
+// export const environment: MaEnvironmentBase = {
+// 	production: true,
+// 	apiBaseUrl: 'https://di.murphyauto.net',
+// 	httpOptions: { headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*' }) },
+// 	googleAnalytics: {
+// 		domain: 'auto',
+// 		trackingId: 'UA-10667959-10'
+// 	}
+// };
 export const environment: MaEnvironmentBase = {
-	production: true,
-	apiBaseUrl: 'https://di.murphyauto.net',
+	production: false,
+	apiBaseUrl: 'https://di2.murphyauto.net',
 	httpOptions: { headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*' }) },
 	googleAnalytics: {
-		domain: 'auto',
-		trackingId: 'UA-10667959-10'
+		domain: 'none',
+		trackingId: 'UA-140002525-1',
 	}
 };
