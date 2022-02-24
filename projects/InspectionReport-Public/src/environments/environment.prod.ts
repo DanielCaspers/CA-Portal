270bcd3fd943e57594fd4e96eb650d98905f32f1
Dan Caspers <dan@caspers.dev>
2022-02-23 19:59:57 -0600

    feat(#31 - Angular Upgrade) - Store all builds per environment in dist folder
    Now all builds regardless of staging or prod are always uploaded to CI
    every time, which means no matter where we want to deploy we always get both copies everytime.

--- projects/InspectionReport-Public/src/environments/environment.prod.ts
+++ projects/InspectionReport-Public/src/environments/environment.prod.ts
@@ -1,21 +1,12 @@
 import { HttpHeaders } from '@angular/common/http';
 import { MaEnvironmentBase } from '../../../../environment.model';
 
-// export const environment: MaEnvironmentBase = {
-// 	production: true,
-// 	apiBaseUrl: 'https://di.murphyauto.net',
-// 	httpOptions: { headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*' }) },
-// 	googleAnalytics: {
-// 		domain: 'auto',
-// 		trackingId: 'UA-10667959-10'
-// 	}
-// };
 export const environment: MaEnvironmentBase = {
-	production: false,
-	apiBaseUrl: 'https://di2.murphyauto.net',
+	production: true,
+	apiBaseUrl: 'https://di.murphyauto.net',
 	httpOptions: { headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*' }) },
 	googleAnalytics: {
-		domain: 'none',
-		trackingId: 'UA-140002525-1',
+		domain: 'auto',
+		trackingId: 'UA-10667959-10'
 	}
 };
